cb57d41ffdac28bdae7371a0af2a1fe784b91ba2
butlershop88 <butlershop88@gmail.com>
2025-09-29 16:01:39 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,34 +1,20 @@
-const CACHE_NAME = 'tareas-app-v7';
-const urlsToCache = [
-    './',
-    './index.html',
-    './app.js',
-    './manifest.json',
-    './icon-192.png',
-    './icon-512.png'
-];
-
-// Install Service Worker
+const CACHE_NAME = 'tareas-app-v13'; 
+const urlsToCache = [ './', './index.html', './manifest.json', './icon-192.png', './icon-512.png' ];
 self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => cache.addAll(urlsToCache))
-    );
+    event.waitUntil( caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)) );
+    self.skipWaiting();
+});
+self.addEventListener('activate', (event) => {
+    event.waitUntil( caches.keys().then((cacheNames) => {
+        return Promise.all( cacheNames.map((cache) => { if (cache !== CACHE_NAME) return caches.delete(cache); }) );
+    }) );
+    return self.clients.claim();
+});
+self.addEventListener('fetch', (event) => {
+    event.respondWith( fetch(event.request).catch(() => caches.match(event.request)) );
 });
 
-// Fetch event - serve cached content when offline
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-        )
-    );
-});
+
 
 
 
