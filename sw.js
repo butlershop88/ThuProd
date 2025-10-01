8ac15ba272a2c50e7b457d9c211f10aeda31e826
butlershop88 <butlershop88@gmail.com>
2025-10-01 19:06:50 +0200

    sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,33 @@
-const CACHE_NAME = 'tareas-app-v14'; 
-const urlsToCache = [ './', './index.html', './manifest.json', './icon-192.png', './icon-512.png' ];
-self.addEventListener('install', (event) => {
-    event.waitUntil( caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache)) );
+const CACHE_NAME = 'tareas-app-v19'; 
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './app.js'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
     self.skipWaiting();
 });
-self.addEventListener('activate', (event) => {
-    event.waitUntil( caches.keys().then((cacheNames) => {
-        return Promise.all( cacheNames.map((cache) => { if (cache !== CACHE_NAME) return caches.delete(cache); }) );
-    }) );
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+                          .map(cacheName => caches.delete(cacheName))
+            );
+        })
+    );
     return self.clients.claim();
 });
-self.addEventListener('fetch', (event) => {
-    event.respondWith( fetch(event.request).catch(() => caches.match(event.request)) );
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
 });
-
-
-
-
-
-
-
-
